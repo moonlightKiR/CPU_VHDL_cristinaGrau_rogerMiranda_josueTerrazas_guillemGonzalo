bf57b8644fa534648eb611523f10d4e8ea94a1a9
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-04-14 17:06:24 +0200

    Fix: reset resets all registers; Feature: async reset

--- BancRegistres_codi.vhd
+++ BancRegistres_codi.vhd
@@ -16,14 +16,14 @@ type RegistroFile is array(0 to 15) of std_logic_vector (15 downto 0);
 signal Registro : RegistroFile;
 begin
 
-	identifier : process (clock)
+	identifier : process (reset, clock)
 	begin
+		if (reset = '0') then
+			Registro <= (others => '0');
+		end if;
 		if (rising_edge(clock)) then
-			if (readwrite = '1' and Address /= "0000" and reset ='1') then
+			if (readwrite = '1' and Address /= "0000") then
 				Registro(to_integer(unsigned(Address))) <= valueIn;
-			end if;
-			if (reset = '0' and readwrite = '1') then
-				Registro(to_integer(unsigned(Address))) <= "0000000000000000";
 			end if;
             valueOut <= Registro(to_integer(unsigned(Address)));
 		end if;
