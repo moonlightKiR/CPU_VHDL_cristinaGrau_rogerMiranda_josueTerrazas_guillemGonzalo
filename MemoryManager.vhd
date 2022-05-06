0ff63ab9a6cc429c3b72f33da8b64e7884cbab17
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-05-06 16:27:42 +0200

    Fix: MemoryManager skipping some inputs

--- MemoryManager.vhd
+++ MemoryManager.vhd
@@ -65,108 +65,111 @@ begin
 	process (state, clk)
 		variable next_u_accessor : unsigned(ACESSOR_BUS - 1 downto 0);
 	begin
-		case state is
-				when ss =>
-					u_accessor <= to_unsigned(0, ACESSOR_BUS);
-					first_check <= (others => '1');
-					
-					next_state <= s0;
-					
-				when s0 =>
-					if enable(accessor) = '1'
-						and (first_check(accessor) = '1'
-							or nread_write(accessor) /= last_nread_write(accessor)
-							or addr(accessor) /= last_addr(accessor)
-							or (nread_write(accessor) = '1' and write_data(accessor) /= last_write_data(accessor))	-- you've updated the previous setted value
-							or (nread_write(accessor) = '0' and last_addr_writted(accessor) = '1')) then			-- some other state has writted the same address, and you were reading it
-						-- there's changes
-						if nread_write(accessor) = '1' then
-							next_state <= s1; -- write
+		if rising_edge(clk) then
+			case state is
+					when ss =>
+						u_accessor <= to_unsigned(0, ACESSOR_BUS);
+						first_check <= (others => '1');
+						done <= (others => '0');
+						
+						next_state <= s0;
+						
+					when s0 =>
+						if enable(accessor) = '1'
+							and (first_check(accessor) = '1'
+								or nread_write(accessor) /= last_nread_write(accessor)
+								or addr(accessor) /= last_addr(accessor)
+								or (nread_write(accessor) = '1' and write_data(accessor) /= last_write_data(accessor))	-- you've updated the previous setted value
+								or (nread_write(accessor) = '0' and last_addr_writted(accessor) = '1')) then			-- some other state has writted the same address, and you were reading it
+							-- there's changes
+							if nread_write(accessor) = '1' then
+								next_state <= s1; -- write
+							else
+								next_state <= s10; -- read
+							end if;
 						else
-							next_state <= s10; -- read
+							next_state <= s22; -- the value that you'll get it's the same; skip
 						end if;
-					else
-						next_state <= s22; -- the value that you'll get it's the same; skip
-					end if;
-					
-				when s1 =>
-					done(accessor) <= '0';
-					w_addr <= addr(accessor);
-					w_data <= write_data(accessor);
-					
-					next_state <= s2;
-					
-				when s2 =>
-					w_addr <= addr(accessor);
-					w_data <= write_data(accessor);
-					w_addr_valid <= '1';
-					w_data_valid <= '1';
-					
-					if w_resp_valid then
-						next_state <= s3;
-					end if;
-					
-				when s3 =>
-					response(accessor) <= w_resp;
-					w_addr_valid <= '0';
-					w_data_valid <= '0';
-					last_write_data(accessor) <= write_data(accessor);
-					
-					-- update last_addr_written
-					for i in 0 to MEMORY_ACCESSES - 1 loop
-						if enable(i) = '1' and i /= accessor 				-- ha de ser un altre entrada, estar activada...
-								and last_addr(i) = addr(accessor) then		-- ... i llegir de la mateixa adreça
-							last_addr_writted(i) <= '1';
+						
+					when s1 =>
+						done(accessor) <= '0';
+						w_addr <= addr(accessor);
+						w_data <= write_data(accessor);
+						
+						next_state <= s2;
+						
+					when s2 =>
+						w_addr <= addr(accessor);
+						w_data <= write_data(accessor);
+						w_addr_valid <= '1';
+						w_data_valid <= '1';
+						
+						if w_resp_valid then
+							next_state <= s3;
 						end if;
-					end loop;
+						
+					when s3 =>
+						response(accessor) <= w_resp;
+						w_addr_valid <= '0';
+						w_data_valid <= '0';
+						last_write_data(accessor) <= write_data(accessor);
+						
+						-- update last_addr_written
+						for i in 0 to MEMORY_ACCESSES - 1 loop
+							if enable(i) = '1' and i /= accessor 				-- ha de ser un altre entrada, estar activada...
+									and last_addr(i) = addr(accessor) then		-- ... i llegir de la mateixa adreça
+								last_addr_writted(i) <= '1';
+							end if;
+						end loop;
+						
+						next_state <= s20;
+						
+					when s10 =>
+						done(accessor) <= '0';
+						r_addr <= addr(accessor);
+						
+						next_state <= s11;
+						
+					when s11 =>
+						r_addr <= addr(accessor);
+						r_addr_valid <= '1';
+						
+						if r_data_valid then
+							next_state <= s12;
+						end if;
+						
+					when s12 =>
+						response(accessor) <= r_resp;
+						read_data(accessor) <= r_data;
+						r_addr_valid <= '0';
+						
+						next_state <= s20;
+						
+					when s20 =>
+						first_check(accessor) <= '0';
+						last_addr_writted(accessor) <= '0';
+						last_nread_write(accessor) <= nread_write(accessor);
+						last_addr(accessor) <= addr(accessor);
+						
+						next_state <= s21;
 					
-					next_state <= s20;
+					when s21 =>
+						done(accessor) <= '1';
+						
+						next_state <= s22;
 					
-				when s10 =>
-					done(accessor) <= '0';
-					r_addr <= addr(accessor);
-					
-					next_state <= s11;
-					
-				when s11 =>
-					r_addr <= addr(accessor);
-					r_addr_valid <= '1';
-					
-					if r_data_valid then
-						next_state <= s12;
-					end if;
-					
-				when s12 =>
-					response(accessor) <= r_resp;
-					read_data(accessor) <= r_data;
-					r_addr_valid <= '0';
-					
-					next_state <= s20;
-					
-				when s20 =>
-					first_check(accessor) <= '0';
-					last_addr_writted(accessor) <= '0';
-					last_nread_write(accessor) <= nread_write(accessor);
-					last_addr(accessor) <= addr(accessor);
-					
-					next_state <= s21;
-				
-				when s21 =>
-					done(accessor) <= '1';
-					
-					next_state <= s22;
-				
-				when s22 =>
-					-- check the next access
-					next_u_accessor := u_accessor+1;
-					if to_integer(next_u_accessor) = MEMORY_ACCESSES then
-						u_accessor <= to_unsigned(0, ACESSOR_BUS); -- last one => start again
-					else
-						u_accessor <= next_u_accessor;
-					end if;
-					
-					next_state <= s0;
-		end case;
+					when s22 =>
+						-- check the next access
+						next_u_accessor := u_accessor+1;
+						if to_integer(next_u_accessor) = MEMORY_ACCESSES then
+							u_accessor <= to_unsigned(0, ACESSOR_BUS); -- last one => start again
+						else
+							u_accessor <= next_u_accessor;
+						end if;
+						
+						next_state <= s0;
+			end case;
+		end if;
 	end process;
 	
 	process (clk)
